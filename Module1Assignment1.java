74b6088bd8b66eae153ee1bcb36ea266a5fb5d7a
ObanCH12 <72536042+ObanCH12@users.noreply.github.com>
2021-02-15 11:05:48 -0500

    Add files via upload

---
+++ Module1Assignment1.java
@@ -0,0 +1,15 @@
+
+package module1assignment1;
+
+public class Module1Assignment1 {
+
+    
+    public static void main(String[] args) {
+        for(int i = 0; i < 101; i++){ //setup for loop so it will end when i is 100 
+            if( 0 == i % 2){ //checks if i's value is a even number or not
+                System.out.println(i); //prints out the value of i if i's value is a sum
+            }
+        }
+    }
+    
+}
